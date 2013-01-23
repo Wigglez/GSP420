73a9a4d2352440912970982abf48f48d7c6e23b5
Wigglez <lod_damageinc@hotmail.com>
2013-01-23 11:05:09 -0600

    Initial commit
    Contains typedefs and empty header files for each core.

---
+++ Typedefs.h
@@ -0,0 +1,20 @@
+#include <string>
+#include <Windows.h>
+
+using std::string;
+
+typedef bool Bool;
+typedef BYTE Byte;
+typedef char Char;
+typedef double Double;
+typedef float Float;
+typedef int Int;
+typedef __int64 Int64;
+typedef long Long;
+typedef string String;
+typedef void Void;
+typedef wchar_t Wchar_t;
+typedef unsigned int uInt;
+typedef unsigned __int64 uInt64;
+typedef unsigned long uLong;
+typedef FILE File;
